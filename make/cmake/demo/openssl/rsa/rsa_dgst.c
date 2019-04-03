9d9fe2adaaf51fe8a94cdcea3cd5d58dae705aa8
kiddlu <kidd.dawny.lu@gmail.com>
2019-04-03 11:32:17 +0800

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- make/cmake/demo/openssl/rsa/rsa_dgst.c
+++ make/cmake/demo/openssl/rsa/rsa_dgst.c
@@ -3,137 +3,116 @@
 #include <openssl/pem.h>
 #include <openssl/err.h>
 
-RSA* getPrivateKey(char* in_szKeyPath)
+#include "file.h"
+
+RSA* get_private_key(char* path)
 {
     FILE    *fp = NULL; 
-    char    szKeyPath[1024];
-    RSA     *priRsa = NULL, *pubRsa = NULL, *pOut = NULL;
+    char    key_path[1024];
+    RSA     *priv_key = NULL;
 
-    memset(szKeyPath, 0 ,sizeof(szKeyPath));
+    memset(key_path, 0 ,sizeof(key_path));
 
-    if(256 < strlen(in_szKeyPath))
-        strncpy(szKeyPath, in_szKeyPath, 256);
+    if(256 < strlen(path))
+        strncpy(key_path, path, 256);
     else
-        strncpy(szKeyPath, in_szKeyPath, strlen(in_szKeyPath));
+        strncpy(key_path, path, strlen(path));
 
-    printf("密钥文件路径[%s]", szKeyPath);
-
-    /*  打开密钥文件 */
-    if(NULL == (fp = fopen(szKeyPath, "rb")))
+    if(NULL == (fp = fopen(key_path, "rb")))
     {
-        printf( "打开密钥文件[%s]出错", szKeyPath);
+        printf( "open key error[%s]\n", key_path);
         return NULL;
     }
-    /*  获取私密钥 */
-    if(NULL == (priRsa = PEM_read_RSAPrivateKey(fp, &priRsa, NULL,NULL)))
+
+    if(NULL == (priv_key = PEM_read_RSAPrivateKey(fp, &priv_key, NULL,NULL)))
     {
-        printf( "读出私钥内容出错\n");
+        printf( "read key error\n");
         fclose(fp);
         return NULL;
     }
     fclose(fp);
 
-    printf("提取私钥\n");
-    pOut = priRsa;
-    return pOut;
+    return priv_key;
 }
 
-RSA* getPublicKey(char* in_szKeyPath)
+RSA* get_public_key(char* path)
 {
     FILE    *fp = NULL; 
-    char    szKeyPath[1024];
-    RSA     *priRsa = NULL, *pubRsa = NULL, *pOut = NULL;
+    char    key_path[1024];
+    RSA     *pub_key = NULL;
 
-    memset(szKeyPath, 0 ,sizeof(szKeyPath));
+    memset(key_path, 0 ,sizeof(key_path));
 
-    if(256 < strlen(in_szKeyPath))
-        strncpy(szKeyPath, in_szKeyPath, 256);
+    if(256 < strlen(path))
+        strncpy(key_path, path, 256);
     else
-        strncpy(szKeyPath, in_szKeyPath, strlen(in_szKeyPath));
+        strncpy(key_path, path, strlen(path));
 
-    printf("密钥文件路径[%s]", szKeyPath);
-
-    /*  打开密钥文件 */
-    if(NULL == (fp = fopen(szKeyPath, "rb")))
-    {
-        printf( "打开密钥文件[%s]出错", szKeyPath);
+    if(NULL == (fp = fopen(key_path, "rb"))) {
+        printf( "open key error[%s]\n", key_path);
         return NULL;
     }
-    /*  获取公密钥 */
-    if(NULL == (priRsa = PEM_read_RSA_PUBKEY(fp, &priRsa, NULL,NULL)))
-    {
-        printf("读出私钥内容出错\n");
+    if(NULL == (pub_key = PEM_read_RSA_PUBKEY(fp, &pub_key, NULL,NULL))) {
+        printf("read key error\n");
         fclose(fp);
         return NULL;
     }
     fclose(fp);
-    printf("提取公钥\n");
-    pOut = priRsa;
-    return pOut;
+    return pub_key;
 }
+
+#define MD_SHA1_LEN       20
+#define MD_SHA256_LEN     32
+#define MAX_SIGNATURE_LEN 2048
 
 int main(void)
 {
-    int     flen,rsa_len, ienLen, iRet;
-    RSA     *prsa = NULL;
-    char    szEnData[]="orderId=01010500201502000004reqTime=20150205012727ext=20151120ext2=1";
-    char    szTmp[10240], szTmp1[10240];
+    int     ret;
+    int     sig_len;
+    RSA     *rsa_content = NULL;
 
-    if(NULL == (prsa = getPrivateKey("rsa_priv.pem")))
-    {
-        RSA_free(prsa);
-        printf("获取私钥失败\n");
+    char    sha_buf[MD_SHA1_LEN];
+    char    signature[MAX_SIGNATURE_LEN];
+
+    if( NULL == (rsa_content = get_private_key("rsa_priv.pem"))) {
+        RSA_free(rsa_content);
+        printf("get priv key error\n");
         return -1;
     }
 
-//  RSA_print_fp(stdout, prsa, 11);
-    flen = strlen(szEnData);
-        printf("待签名数据:[%s]\n", szEnData);
+    memset(sha_buf, 0, sizeof(sha_buf));
+    memset(signature, 0, sizeof(signature));
 
-    memset(szTmp, 0, sizeof(szTmp));
-    memset(szTmp1, 0, sizeof(szTmp1));
-    //  对待签名数据做SHA1摘要
-    SHA1(szEnData, flen, szTmp);
-    //使用私钥对SHA1摘要做签名
-    ienLen = RSA_sign(NID_sha1, (unsigned char *)szTmp, 20, (unsigned char*)szTmp1, &iRet, prsa);
-    if(ienLen != 1 )
-    {
-            printf("签名失败\n");
-            RSA_free(prsa);
+    SHA1(file_buf, file_len, sha_buf);
+
+    ret = RSA_sign(NID_sha1, (unsigned char *)sha_buf, MD_SHA1_LEN, (unsigned char*)signature, &sig_len, rsa_content);
+    if(ret != 1 ) {
+            printf("rsa sign error\n");
+            RSA_free(rsa_content);
             return -1;
-        }
-        RSA_free(prsa);
-        printf("签名成功\n");
-    //签名串szTmp1二进制数据需要转成base64编码
-    //mac=base64encode(szTmp1)这是伪码,生产MAC值，给对方去校验
+    }
 
+    RSA_free(rsa_content);
 
-
-    //验证签名
-    //验证签名的是需要获取MAC值，明文签名数据,对“明文签名数据”做SHA1，获得摘要。在对MAC做basedecode(mac)，然后调用函数验证签名
-    if(NULL == (prsa = getPublicKey("rsa_pub.pem")))
-    {
-        RSA_free(prsa);
-        printf("获取私钥失败\n");
+    if(NULL == (rsa_content = get_public_key("rsa_pub.pem"))) {
+        RSA_free(rsa_content);
+        printf("get pub key error\n");
         return -1;
     }
-    flen = strlen(szEnData);
-    printf("待签名数据:[%s]\n", szEnData);//签名数据 和 mac 因该是由通信报文中获得，这里演示直接用使用同一变量
 
-    memset(szTmp, 0, sizeof(szTmp));
-    memset(szTmp1, 0, sizeof(szTmp1));
-    //  对待签名数据做SHA1摘要
-    SHA1(szEnData, flen, szTmp);
+    memset(sha_buf, 0, sizeof(sha_buf));
 
-    ienLen = RSA_verify(NID_sha1, (unsigned char *)szTmp, 20, (unsigned char*)szTmp1, iRet, prsa);
-    if(ienLen != 1 )
-    {
-            printf("签名不合法\n");
-            RSA_free(prsa);
+    SHA1(file_buf, file_len, sha_buf);
+
+    ret = RSA_verify(NID_sha1, (unsigned char *)sha_buf, MD_SHA1_LEN, (unsigned char*)signature, sig_len, rsa_content);
+    if(ret != 1 ) {
+            printf("verify error\n");
+            RSA_free(rsa_content);
             return -1;
+    } else {
+        printf("verify ok\n");
+        RSA_free(rsa_content);
     }
-    else
-            printf("验签成功\n");
-    RSA_free(prsa);
+
     return 0;
 }
